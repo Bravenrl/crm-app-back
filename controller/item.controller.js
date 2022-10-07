24e9bec7845a62e6c2a3eb605abef3d2076593e4
Bravenrl <bravenrl@gmail.com>
2022-10-08 01:07:50 +0300

    add create item

--- controller/item.controller.js
+++ controller/item.controller.js
@@ -3,7 +3,8 @@ const db = require('../db')
 class ItemController {
   async create(req, res) {
     const { id, date, name, count, distance } = req.body;
-    res.json('OK');
+    const newItem = await db.query('INSERT INTO items_table (id, date, name, count, distance) VALUES ($1, $2, $3, $4, $5) RETURNING *', [id, date, name, count, distance])
+    res.json(newItem.rows[0]);
   }
 
   async getAll(req, res) {
