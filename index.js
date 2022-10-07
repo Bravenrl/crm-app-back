88dc3a7590f4327e71b403bd2e9280b00e038150
Bravenrl <bravenrl@gmail.com>
2022-10-08 02:26:19 +0300

    allow cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express =  require('express');
+const express = require('express');
+const cors = require('cors');
 const itemRouter = require('./routes/item.routes');
 
 const PORT = process.env.PORT || 4200;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/', itemRouter);
 
